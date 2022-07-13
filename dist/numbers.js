4a728ef23068ede3f493de802f810f6b69051f44
Carlos Ferreira <carlos@especializati.com.br>
2022-07-13 15:36:41 -0300

    TS Numbers

---
+++ dist/numbers.js
@@ -0,0 +1,12 @@
+"use strict";
+let example;
+example = 123; // inteiro
+console.log(example);
+example = 12.2; // ponto flutuante
+console.log(example);
+example = 0x37CF; // hexadicimal
+console.log(example);
+example = 0o377; // octal
+console.log(example);
+example = 0b111001; // binary
+console.log(example);
---
+++ src/numbers.ts
@@ -0,0 +1,12 @@
+let example: number;
+
+example = 123; // inteiro
+console.log(example);
+example = 12.2; // ponto flutuante
+console.log(example);
+example = 0x37CF; // hexadicimal
+console.log(example);
+example = 0o377; // octal
+console.log(example);
+example = 0b111001; // binary
+console.log(example);
