6fa809b88397ebbff284f11710a135a1f0a767e2
Carlos Ferreira <carlos@especializati.com.br>
2022-07-13 15:52:06 -0300

    TS object

---
+++ dist/object.js
@@ -0,0 +1,16 @@
+"use strict";
+let employee;
+employee = {
+    firstName: 'Carlos',
+    lastName: 'Ferreira',
+    score: 90,
+    isActive: false
+};
+console.log(employee);
+let employee2 = {
+    firstName: 'Carlos',
+    lastName: 'Ferreira',
+    score: 900,
+    isActive: true
+};
+console.log(employee2.firstName);
---
+++ src/object.ts
@@ -0,0 +1,23 @@
+let employee: object;
+
+employee = {
+  firstName: 'Carlos',
+  lastName: 'Ferreira',
+  score: 90,
+  isActive: false
+}
+console.log(employee);
+
+let employee2: {
+  firstName: string;
+  lastName: string;
+  score: number;
+  isActive: boolean;
+} = {
+  firstName: 'Carlos',
+  lastName: 'Ferreira',
+  score: 900,
+  isActive: true
+};
+
+console.log(employee2.firstName);
