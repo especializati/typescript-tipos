07763cdffbcca9317263453d305308af04293070
Carlos Ferreira <carlos@especializati.com.br>
2022-07-13 15:40:27 -0300

    TS string

---
+++ dist/string.js
@@ -0,0 +1,6 @@
+"use strict";
+let firstName;
+let lastName = 'Ferreira';
+firstName = 'Carlos';
+const fullName = `${firstName} ${lastName}`;
+console.log(fullName.toUpperCase());
---
+++ src/string.ts
@@ -0,0 +1,7 @@
+let firstName: string;
+let lastName: string = 'Ferreira';
+
+firstName = 'Carlos';
+
+const fullName = `${firstName} ${lastName}`;
+console.log(fullName.toUpperCase());
