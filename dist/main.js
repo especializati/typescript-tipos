44e596ac92df6a628c7897c55e3bcf5bc5386a5e
Carlos Ferreira <carlos@especializati.com.br>
2022-07-13 15:31:00 -0300

    Intro

---
+++ dist/main.js
@@ -0,0 +1,5 @@
+"use strict";
+// Type Annotations
+// let name: string;
+// let name: string = 'Carlos';
+Object.defineProperty(exports, "__esModule", { value: true });
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
+// Type Annotations
+// let name: string;
+// let name: string = 'Carlos';
 
+// Type Inference
+// let age = 90;
+
+export {}
