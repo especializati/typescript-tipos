9399f9234506e12f694af9af08e43325f7356a6c
Carlos Ferreira <carlos@especializati.com.br>
2022-07-13 17:54:25 -0300

    TS any

---
+++ dist/any.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// evite ao máximo utilizar o type any
+const showLog = (message) => {
+    console.log(message);
+};
+showLog('test');
---
+++ src/any.ts
@@ -0,0 +1,7 @@
+// evite ao máximo utilizar o type any
+const showLog = (message: any): void => {
+  console.log(message);
+}
+showLog('test');
+
+export {}
