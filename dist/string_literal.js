f1b8f74f7453f51ea622344f4ea05b448a863b43
Carlos Ferreira <carlos@especializati.com.br>
2022-07-13 17:04:46 -0300

    TS string literal

---
+++ dist/string_literal.js
@@ -0,0 +1,5 @@
+"use strict";
+let mouseEvent;
+mouseEvent = 'mouseup';
+// mouseEvent = 'mouseover';
+console.log(mouseEvent);
---
+++ src/string_literal.ts
@@ -0,0 +1,6 @@
+let mouseEvent: 'click' | 'mouseup' | 'mousedown' | 'dblclick'
+
+mouseEvent = 'mouseup';
+// mouseEvent = 'mouseover';
+
+console.log(mouseEvent);
