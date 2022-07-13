ec7994479c421380fa9420fe1ee2baecc833e197
Carlos Ferreira <carlos@especializati.com.br>
2022-07-13 16:07:38 -0300

    TS tuples

---
+++ dist/tuples.js
@@ -0,0 +1,9 @@
+"use strict";
+const userData = ['Carlos', 900];
+console.log(userData);
+let users;
+users = [
+    ['Carlos', 'Ferreira', 900, true],
+    ['Especializa', 'TI', 950, true],
+];
+console.log(users);
---
+++ src/tuples.ts
@@ -0,0 +1,9 @@
+const userData: [string, number] = ['Carlos', 900];
+console.log(userData);
+
+let users: [string, string, number, boolean][];
+users = [
+  ['Carlos', 'Ferreira', 900, true],
+  ['Especializa', 'TI', 950, true],
+];
+console.log(users);
