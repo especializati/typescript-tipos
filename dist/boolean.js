2c83a9d0625117dc1dc8a5da873e4a527586d887
Carlos Ferreira <carlos@especializati.com.br>
2022-07-13 15:44:02 -0300

    TS boolean

---
+++ dist/boolean.js
@@ -0,0 +1,8 @@
+"use strict";
+let isActive = true;
+isActive = false;
+console.log(isActive);
+// isActive = 'sdfs';
+let published;
+published = true;
+console.log(published);
---
+++ src/boolean.ts
@@ -0,0 +1,8 @@
+let isActive = true;
+isActive = false;
+console.log(isActive);
+// isActive = 'sdfs';
+
+let published: boolean;
+published = true;
+console.log(published);
