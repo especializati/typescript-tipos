47f791b976368fcd407c3218f47d50ac4dbabb19
Carlos Ferreira <carlos@especializati.com.br>
2022-07-13 15:58:57 -0300

    TS arrays

---
+++ dist/arrays.js
@@ -0,0 +1,10 @@
+"use strict";
+// let megacena: number[];
+let megacena;
+megacena = [1, 2, 3, 45, 6];
+console.log(megacena);
+// let arr = [1,2,'string', true, 1.2];
+// let ids: Array<number | string>;
+let ids;
+ids = [1, 'string'];
+console.log(ids);
---
+++ src/arrays.ts
@@ -0,0 +1,11 @@
+// let megacena: number[];
+let megacena: Array<number>;
+megacena = [1,2,3,45,6];
+console.log(megacena);
+
+// let arr = [1,2,'string', true, 1.2];
+
+// let ids: Array<number | string>;
+let ids: (number | string)[];
+ids = [1, 'string'];
+console.log(ids);
